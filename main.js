0ef25d923510cc3cfdd23b08df725b1c63575edd
Tekaso <48576171+Tekaso-os@users.noreply.github.com>
2023-05-04 10:59:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+form.addEventListener('submit', function(e){
+    const numa = document.getElementById("numero-a");
+const numb = document.getElementById("numero-b");
+const form = document.getElementById("formn")
+e.prefentDefault();
+    alert(numa + numb)
+})
+console.log(form)
